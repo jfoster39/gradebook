1f9fe912aaaef6c10b5ae87b5fe2a5fdcafe1de5
Tyler Foster <jfoster39@gatech.edu>
2013-07-31 15:32:22 -0400

    sdf

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,34 @@
+package gradebook.model;
+import org.junit.Test;
+import java.util.*;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+    
+
+    @Test
+    public void testSetScores() throws Exception {
+        
+    }
+
+    @Test
+    public void testCalculateAverageScore() throws Exception {
+
+    }
+
+    @Test
+    public void testCalculateLetterGrade() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAverageScore() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLetterGrade() throws Exception {
+
+    }
+}
