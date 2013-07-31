0b45645554351f823251144feb3cb2e1294f98f2
Tyler Foster <jfoster39@gatech.edu>
2013-07-31 15:41:49 -0400

    tests work

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,15 +1,27 @@
-package gradebook.model;
+import gradebook.model.*;
 import org.junit.Test;
 import java.util.*;
 import static org.junit.Assert.*;
 
 public class StudentTest {
 
-    
+    GradebookItem test1 = new GradebookItem("test1", 100);
+    GradebookItem quiz1 = new GradebookItem("quiz1", 80);
+    ArrayList<GradebookItem> testItems = new ArrayList();
+    ArrayList<GradebookItem> quizItems = new ArrayList();
+    GradebookCategory tests = new GradebookCategory("tests", 4, testItems);
+    GradebookCategory quizzes = new GradebookCategory("quizzes", 2, quizItems);
+    ArrayList categories = new ArrayList();
+    Student student1 = new Student("dave", categories);
 
     @Test
     public void testSetScores() throws Exception {
-        
+        testItems.add(test1);
+        quizItems.add(quiz1);
+        ArrayList categories = new ArrayList();
+        categories.add(tests);
+        categories.add(quizzes);
+        ArrayList scores = student1.setScores();
     }
 
     @Test
